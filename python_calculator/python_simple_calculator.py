f6f7edf28dc1cc36efc34f8b48ed50aa6c2f0a86
unknown <evikody06@gmail.com>
2024-10-01 14:19:35 +0800

    The colors has been defined

--- python_calculator/python_simple_calculator.py
+++ python_calculator/python_simple_calculator.py
@@ -8,3 +8,9 @@ pygame.init()
 WIDTH, HEIGHT = 500, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simple Calculator")
+
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+GREEN = (0, 255, 0)
